df0e7d4b2c26925e4393449194634321fc517768
Hoang Tran <hoatran@student.hive.fi>
2024-02-09 18:26:52 +0200

    Added ft_strdup_chr() function

--- libs/libft/string/ft_strdup_chr.c
+++ libs/libft/string/ft_strdup_chr.c
@@ -1,27 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_string.h                                     :+:      :+:    :+:   */
+/*   ft_strdup_chr.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/18 15:37:34 by hoatran           #+#    #+#             */
-/*   Updated: 2024/02/09 18:25:16 by hoatran          ###   ########.fr       */
+/*   Created: 2024/02/09 18:12:21 by hoatran           #+#    #+#             */
+/*   Updated: 2024/02/09 18:25:00 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_STRING_H
-# define LIBFT_STRING_H
+#include <stddef.h>
 
-# include "../common/libft_common.h"
-
-typedef struct s_string
+char	*ft_strdup_chr(char *source, char delimiter)
 {
-	char	*content;
 	size_t	length;
-}	t_string;
+	char	*dup;
 
-t_string	*ft_string(char *str);
-char		*ft_strdup_chr(char *source, char delimiter);
-
-#endif
+	length = 0;
+	while (source[length] != '\0' && source[length] != delimiter)
+		length++;
+	dup = (char *)malloc((length + 1) * sizeof(char));
+	length = 0;
+	while (source[length] != '\0' && source[length] != delimiter)
+	{
+		dup[length] = source[length];
+		length++;
+	}
+	dup[length] = '\0';
+	return (dup);
+}
