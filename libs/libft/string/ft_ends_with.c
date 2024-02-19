8f044eac88a952204b4cc838fc9ac09c51ebc0f1
Hoang Tran <hoang.tran.fin@gmail.com>
2024-02-19 23:40:44 +0200

    Removed unused size_t i

--- libs/libft/string/ft_ends_with.c
+++ libs/libft/string/ft_ends_with.c
@@ -6,7 +6,7 @@
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 00:42:48 by hoatran           #+#    #+#             */
-/*   Updated: 2024/02/19 01:02:58 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/02/19 23:16:29 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ t_bool	ft_ends_with(const char *string, const char *search_string)
 {
 	size_t		ss_length;
 	size_t		string_length;
-	size_t		i;
 	const char	*temp;
 
 	ss_length = ft_strlen(search_string);
@@ -25,7 +24,6 @@ t_bool	ft_ends_with(const char *string, const char *search_string)
 	string_length = ft_strlen(string);
 	if (ss_length > string_length)
 		return (false);
-	i = 0;
 	temp = string + string_length - ss_length;
 	while (*temp != '\0' && *search_string != '\0')
 	{
