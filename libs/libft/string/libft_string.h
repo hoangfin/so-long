a64b0a03e2e2b806fff13e3cbbf255ec38790be1
Hoang Tran <hoang.tran.fin@gmail.com>
2024-02-20 20:16:46 +0200

    Removed ft_grid_print

--- libs/libft/string/libft_string.h
+++ libs/libft/string/libft_string.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft_string.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:37:34 by hoatran           #+#    #+#             */
-/*   Updated: 2024/02/20 17:46:46 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/02/20 20:16:01 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,5 @@ t_bool		ft_ends_with(const char *string, const char *search_string);
 char		*ft_strdup_chr(char *source, char delimiter);
 char		**ft_matrix(size_t row_count, size_t col_count, int c);
 void		ft_matrix_delete(char ***matrix);
-void		ft_grid_print(t_grid *grid);
 
 #endif
