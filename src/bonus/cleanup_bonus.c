8d22d1b3cb3f31acbdc4143959ba3394c0d2ee18
Hoang Tran <hoang.tran.fin@gmail.com>
2024-02-23 20:46:44 +0200

    Delete player-enemy-collectible sprites

--- src/bonus/cleanup_bonus.c
+++ src/bonus/cleanup_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cleanup_bonus.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 22:19:24 by hoatran           #+#    #+#             */
-/*   Updated: 2024/02/22 17:09:24 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/02/23 20:45:50 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 void	cleanup(t_game *game)
 {
 	ft_matrix_delete(&game->map);
+	delete_sprite(game->mlx, &game->player_sprite);
+	delete_sprite(game->mlx, &game->enemy_sprite);
+	delete_sprite(game->mlx, &game->collectible_sprite);
 	mlx_delete_image(game->mlx, game->space);
 	mlx_delete_image(game->mlx, game->wall);
 	mlx_delete_image(game->mlx, game->player);
