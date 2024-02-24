9ddbf42b3a3834deb96576e46078b6605a250cbc
Hoang Tran <hoang.tran.fin@gmail.com>
2024-02-24 20:08:22 +0200

    Added static variables accumulator and current_frame

--- src/bonus/update_collectibles_bonus.c
+++ src/bonus/update_collectibles_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   update_collectibles_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 11:45:09 by hoatran           #+#    #+#             */
-/*   Updated: 2024/02/24 18:16:27 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/02/24 20:06:02 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,21 @@
 void	update_collectibles(void *param, double elapsed_time)
 {
 	static double	accumulator = 0;
-	static int		current_frame = 0;
+	static int32_t	current_frame = 0;
 	t_game *const	game = (t_game *)param;
+	int32_t			frame_index;
 
 	accumulator += elapsed_time;
+	frame_index = current_frame % game->collectible_sprite->col_count;
 	if (accumulator > 0.1)
 	{
 		put_pixel(
 			game->collectible,
 			game->collectible_sprite->image,
-			(current_frame % game->collectible_sprite->col_count) * 128,
+			frame_index * game->collectible_sprite->frame_w,
 			0 \
 		);
 		current_frame++;
 		accumulator -= 0.1;
 	}
-	printf("update_collectibles\n");
 }
