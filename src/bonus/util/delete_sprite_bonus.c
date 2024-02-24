890c37e0dd9a62c032bf7f2fdb9afd3f7f309fa3
Hoang Tran <hoang.tran.fin@gmail.com>
2024-02-24 23:31:38 +0200

    Guard against NULL

--- src/bonus/util/delete_sprite_bonus.c
+++ src/bonus/util/delete_sprite_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 20:31:00 by hoatran           #+#    #+#             */
-/*   Updated: 2024/02/23 20:55:59 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/02/24 23:30:54 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	delete_sprite(mlx_t *mlx, t_sprite **sprite)
 {
+	if (*sprite == NULL)
+		return ;
 	mlx_delete_image(mlx, (*sprite)->image);
 	free(*sprite);
 	*sprite = NULL;
