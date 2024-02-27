612c60c4d2c0b03de930ce0fcc2549c4ab1df465
Hoang Tran <hoatran@student.hive.fi>
2024-02-27 14:32:09 +0200

    Added static reset()

--- src/bonus/player/transition_player_bonus.c
+++ src/bonus/player/transition_player_bonus.c
@@ -3,35 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   transition_player_bonus.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 15:27:26 by hoatran           #+#    #+#             */
-/*   Updated: 2024/02/26 20:50:15 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/02/27 13:56:37 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long_bonus.h"
 
-t_player_state	transition_player(t_player_state current_state, keys_t key)
+static void	reset(t_character *player)
 {
-	if (current_state == PLAYER_IDLE)
+	player->current_frame = 0;
+	player->distance_acc = 0;
+	player->elapsed_time_acc = 0;
+}
+
+t_player_state	transition_player(t_character *player, keys_t key)
+{
+	if (player->state == PLAYER_IDLE)
 	{
 		if (key == MLX_KEY_W)
-			return (PLAYER_MOVE_UP);
+			return (reset(player), PLAYER_MOVE_UP);
 		if (key == MLX_KEY_D)
-			return (PLAYER_MOVE_RIGHT);
+			return (reset(player), PLAYER_MOVE_RIGHT);
 		if (key == MLX_KEY_S)
-			return (PLAYER_MOVE_DOWN);
+			return (reset(player), PLAYER_MOVE_DOWN);
 		if (key == MLX_KEY_A)
-			return (PLAYER_MOVE_LEFT);
+			return (reset(player), PLAYER_MOVE_LEFT);
 	}
-	if (current_state == PLAYER_MOVE_UP)
+	if (player->state == PLAYER_MOVE_UP)
 		return (PLAYER_MOVE_UP);
-	if (current_state == PLAYER_MOVE_RIGHT)
+	if (player->state == PLAYER_MOVE_RIGHT)
 		return (PLAYER_MOVE_RIGHT);
-	if (current_state == PLAYER_MOVE_DOWN)
+	if (player->state == PLAYER_MOVE_DOWN)
 		return (PLAYER_MOVE_DOWN);
-	if (current_state == PLAYER_MOVE_LEFT)
+	if (player->state == PLAYER_MOVE_LEFT)
 		return (PLAYER_MOVE_LEFT);
-	return (current_state);
+	return (player->state);
 }
