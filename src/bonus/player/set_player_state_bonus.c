874d41a101708d68ee97dacbe844104432d62d1d
Hoang Tran <hoatran@student.hive.fi>
2024-02-28 12:23:47 +0200

    Fixed include path

--- src/bonus/player/set_player_state_bonus.c
+++ src/bonus/player/set_player_state_bonus.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   set_player_state_bonus.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 22:51:10 by hoatran           #+#    #+#             */
-/*   Updated: 2024/02/27 23:41:57 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/02/28 12:22:17 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long_bonus.h"
+#include "../so_long_bonus.h"
 
 void	set_player_state(t_character *player, t_character_state player_state)
 {
