c28d9aca4fdcf8f67eb0ddcdccf4deceb4618cc9
Hoang Tran <hoatran@student.hive.fi>
2024-02-29 14:29:12 +0200

    Support left right idle update

--- src/bonus/player/update_player_bonus.c
+++ src/bonus/player/update_player_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   update_player_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 13:22:59 by hoatran           #+#    #+#             */
-/*   Updated: 2024/02/28 22:59:54 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/02/29 14:27:40 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@ void	update_player(t_character *player, double elapsed_time)
 
 	if (player->distance_moved == RENDER_PIXELS)
 	{
-		set_character_state(player, PLAYER_IDLE);
-		return ;
+		if (player->state == PLAYER_MOVE_LEFT)
+			return (set_character_state(player, PLAYER_IDLE_LEFT));
+		return (set_character_state(player, PLAYER_IDLE_RIGHT));
 	}
 	distance = (int32_t)(player->velocity * elapsed_time);
 	if (player->distance_moved + distance > RENDER_PIXELS)
 		distance = RENDER_PIXELS - player->distance_moved;
-	if (player->state == PLAYER_IDLE)
+	if (player->state == PLAYER_IDLE_LEFT || player->state == PLAYER_IDLE_RIGHT)
 		distance = 0;
 	else if (player->state == PLAYER_MOVE_UP)
 		player->y -= distance;
