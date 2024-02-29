43f417e17d19cab002b5698182689ec28ce0637c
Hoang Tran <hoatran@student.hive.fi>
2024-02-29 14:32:54 +0200

    Redefine animation index for each action

--- src/bonus/player/update_player_ui_bonus.c
+++ src/bonus/player/update_player_ui_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   update_player_ui_bonus.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 16:44:22 by hoatran           #+#    #+#             */
-/*   Updated: 2024/02/28 23:11:06 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/02/29 14:18:44 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 void	update_player_ui(t_character *player, t_sprite *player_sprite)
 {
-	if (player->state == PLAYER_IDLE)
+	if (player->state == PLAYER_IDLE_LEFT)
+		animate_idle(player, player_sprite, 1);
+	else if (player->state == PLAYER_IDLE_RIGHT)
 		animate_idle(player, player_sprite, 0);
 	else if (player->state == PLAYER_MOVE_UP)
-		animate_ver_move(player, player_sprite, 7);
+		animate_ver_move(player, player_sprite, 4);
 	else if (player->state == PLAYER_MOVE_RIGHT)
-		animate_hor_move(player, player_sprite, 1);
+		animate_hor_move(player, player_sprite, 2);
 	else if (player->state == PLAYER_MOVE_DOWN)
 		animate_ver_move(player, player_sprite, 5);
 	else if (player->state == PLAYER_MOVE_LEFT)
-		animate_hor_move(player, player_sprite, 2);
+		animate_hor_move(player, player_sprite, 3);
 }
