b6ffa5c11757b7980dd346df001a2e23a8878c9d
Hoang Tran <hoatran@student.hive.fi>
2024-03-02 16:26:46 +0200

    Update travel distance to 2 pixels per frame

--- src/bonus/enemy/update_enemies_bonus.c
+++ src/bonus/enemy/update_enemies_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 13:22:59 by hoatran           #+#    #+#             */
-/*   Updated: 2024/03/01 17:39:56 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/03/02 15:12:03 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,27 @@
 
 static void	update_enemy(t_character *enemy, double elapsed_time)
 {
-	int32_t		distance;
-
+	if (enemy->is_state_changed)
+	{
+		enemy->is_state_changed = false;
+		return ;
+	}
 	if (enemy->distance_moved == RENDER_PIXELS)
 	{
 		if (enemy->state == ENEMY_MOVE_LEFT)
 			return (set_character_state(enemy, ENEMY_IDLE_LEFT));
 		return (set_character_state(enemy, ENEMY_IDLE_RIGHT));
 	}
-	distance = (int32_t)(enemy->velocity * elapsed_time);
-	if (enemy->distance_moved + distance > RENDER_PIXELS)
-		distance = RENDER_PIXELS - enemy->distance_moved;
-	if (enemy->state == ENEMY_IDLE_LEFT || enemy->state == ENEMY_IDLE_RIGHT)
-		distance = 0;
-	else if (enemy->state == ENEMY_MOVE_UP)
-		enemy->y -= distance;
+	if (enemy->state == ENEMY_MOVE_UP)
+		enemy->y -= 2;
 	else if (enemy->state == ENEMY_MOVE_RIGHT)
-		enemy->x += distance;
+		enemy->x += 2;
 	else if (enemy->state == ENEMY_MOVE_DOWN)
-		enemy->y += distance;
+		enemy->y += 2;
 	else if (enemy->state == ENEMY_MOVE_LEFT)
-		enemy->x -= distance;
-	enemy->distance_moved += distance;
-	enemy->distance_acc += distance;
-	enemy->elapsed_time_acc += elapsed_time;
+		enemy->x -= 2;
+	enemy->distance_moved += 2;
+	enemy->elapsed_time += elapsed_time;
 }
 
 void	update_enemies(t_character **enemies, double elapsed_time)
