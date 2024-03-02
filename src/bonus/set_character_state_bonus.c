7a02330caba0c76d79a956af52094732332986d8
Hoang Tran <hoatran@student.hive.fi>
2024-03-02 16:27:50 +0200

    Removed distance_acc, added is_state_changed

--- src/bonus/set_character_state_bonus.c
+++ src/bonus/set_character_state_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   set_character_state_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 22:51:10 by hoatran           #+#    #+#             */
-/*   Updated: 2024/02/28 22:58:05 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/03/02 14:41:19 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	set_character_state(t_character *ch, t_character_state state)
 {
 	ch->current_frame = 0;
 	ch->distance_moved = 0;
-	ch->distance_acc = 0;
-	ch->elapsed_time_acc = 0;
+	ch->elapsed_time = 0;
+	ch->is_state_changed = true;
 	ch->state = state;
 }
