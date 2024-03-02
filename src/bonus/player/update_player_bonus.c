e4a8213d3d7b136841663f0d3288418a313ed9f0
Hoang Tran <hoatran@student.hive.fi>
2024-03-02 16:26:19 +0200

    Update travel distance to 2 pixels per frame

--- src/bonus/player/update_player_bonus.c
+++ src/bonus/player/update_player_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   update_player_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 13:22:59 by hoatran           #+#    #+#             */
-/*   Updated: 2024/03/02 00:08:43 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/03/02 14:44:59 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,25 @@
 
 void	update_player(t_character *player, double elapsed_time)
 {
-	// int32_t	distance;
-
+	if (player->is_state_changed)
+	{
+		player->is_state_changed = false;
+		return ;
+	}
 	if (player->distance_moved == RENDER_PIXELS)
 	{
 		if (player->state == PLAYER_MOVE_LEFT)
 			return (set_character_state(player, PLAYER_IDLE_LEFT));
 		return (set_character_state(player, PLAYER_IDLE_RIGHT));
 	}
-	// distance = (int32_t)(player->velocity * elapsed_time);
-	// if (player->distance_moved + distance > RENDER_PIXELS)
-	// 	distance = RENDER_PIXELS - player->distance_moved;
-	// if (player->state == PLAYER_IDLE_LEFT || player->state == PLAYER_IDLE_RIGHT)
-	// 	distance = 0;
-	else if (player->state == PLAYER_MOVE_UP)
-		player->y -= 4;
+	if (player->state == PLAYER_MOVE_UP)
+		player->y -= 2;
 	else if (player->state == PLAYER_MOVE_RIGHT)
-		player->x += 4;
+		player->x += 2;
 	else if (player->state == PLAYER_MOVE_DOWN)
-		player->y += 4;
+		player->y += 2;
 	else if (player->state == PLAYER_MOVE_LEFT)
-		player->x -= 4;
-	player->distance_moved += 4;
-	player->distance_acc += 4;
-	player->elapsed_time_acc += elapsed_time;
+		player->x -= 2;
+	player->distance_moved += 2;
+	player->elapsed_time += elapsed_time;
 }
