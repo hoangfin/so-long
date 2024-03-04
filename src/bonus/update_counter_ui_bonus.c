b7788bfc11e1adba8bbb3dbe59b771c7423b6672
Hoang Tran <hoatran@student.hive.fi>
2024-03-04 16:58:55 +0200

    Fixed 0 remains visible when counter is increasing

--- src/bonus/update_counter_ui_bonus.c
+++ src/bonus/update_counter_ui_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hoatran <hoatran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 17:18:36 by hoatran           #+#    #+#             */
-/*   Updated: 2024/03/02 17:35:43 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/03/04 16:53:09 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,22 @@
 void	update_counter_ui(t_game *game)
 {
 	char	buffer[sizeof(int) * 8 + 1];
-	int32_t	x;
-	int32_t	y;
 
-	if (game->move_count_img != NULL)
+	if (game->move_count_img == NULL)
 	{
-		x = game->move_count_img->instances[0].x;
-		y = game->move_count_img->instances[0].y;
-		mlx_delete_image(game->mlx, game->move_count_img);
+		game->move_count_img = mlx_put_string(\
+			game->mlx, \
+			(const char *)ft_itoa(game->move_count, buffer, 10), \
+			175, \
+			PADDING \
+		);
+		return ;
 	}
-	else
-	{
-		x = PADDING + game->text->width + 5;
-		y = PADDING;
-	}
+	mlx_delete_image(game->mlx, game->move_count_img);
 	game->move_count_img = mlx_put_string(\
 		game->mlx, \
 		(const char *)ft_itoa(game->move_count, buffer, 10), \
-		x, \
-		y \
+		175, \
+		PADDING \
 	);
 }
